a90975064de0e2f61b0583ec96b2ddb0c7391f69
waloro <walterof1@hotmail.com>
2020-05-08 21:23:29 -0600

    add interface

---
+++ Proyecto_ED2/Repository/IFixedSizeText.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proyecto_ED2.Repository
+{
+    public interface IFixedSizeText
+    {
+        int FixedSizeText { get; set; }
+        string ToFixedSizeString();
+        string ToNullFormat();
+    }
+}
